331fc64164eb61933c5dcec7cbb4ebe63a113b32
Sirish Renukumar <sirish.renukumar@gmail.com>
2014-05-18 15:27:50 +0530

    Backup commit

--- simple/src/main/java/simple/BeanFactoryExample.java
+++ simple/src/main/java/simple/BeanFactoryExample.java
@@ -11,17 +11,27 @@ public class BeanFactoryExample {
 	public static class Car {
 		Logger logger = Logger.getLogger(Car.class);
 		
-		Car() {
-			logger.info("Car");
-		}
+		private Engine engine;
 		
+		Car(Engine engine) {
+			logger.info("Car");
+			this.engine = engine;
+		}
 	}
 
+	public static class Engine {
+		Logger logger = Logger.getLogger(Engine.class);
+		
+		Engine() {
+			logger.info("Engine");
+		}
+	}
+	
 	public static void main(String [] a) {
 		
 		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
-        reader.loadBeanDefinitions(new ClassPathResource("../../resources/spring/bean-definitions.xml", BeanFactoryExample.class));
+        reader.loadBeanDefinitions(new ClassPathResource("../../resources/spring/BeanFactoryExample.xml", BeanFactoryExample.class));
         
         beanFactory.getBean(Car.class);
 	}
