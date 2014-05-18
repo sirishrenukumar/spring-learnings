ddb8555ee0dc0c6fab942fe4f58885d02921c604
Sirish Renukumar <sirish.renukumar@gmail.com>
2014-05-18 15:45:06 +0530

    Updated example

--- simple/src/main/java/simple/InitializationAndDestructonCallbacksExample.java
+++ simple/src/main/java/simple/InitializationAndDestructonCallbacksExample.java
@@ -1,13 +1,47 @@
 package simple;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class InitializationAndDestructonCallbacksExample {
 	
+	public static class Team implements InitializingBean, DisposableBean{
+		Logger logger = Logger.getLogger(Team.class);
+		
+		void initialize() {
+			logger.info("initialize");
+		}
+		
+		void destruction() {
+			logger.info("destruction");
+		}
+
+		void setup() {
+			logger.info("setup");
+		}
+
+		void teardown() {
+			logger.info("teardown");
+		}
+
+		public void destroy() throws Exception {
+			logger.info("destroy");
+		}
+
+		public void afterPropertiesSet() throws Exception {
+			logger.info("afterPropertiesSet");
+		}
+	}
+	
+	
 	public static void main(String []a) {
 		
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/InitializationAndDestructonCallbacksExample.xml");
+		
+		applicationContext.getBean(Team.class);
 	}
 
 }
